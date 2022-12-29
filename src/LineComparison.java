42e2c0ed56ce7ca6d8b7d947c877376ac5a9573b
Prashant Deepake <deepakeprashant1@gmail.com>
2022-12-29 21:53:39 +0530

    LINE EQUALITY CHECK USE EQUAL METHOD

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,10 +1,22 @@
+import java.util.Objects;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome To Line Comparison Computation");
-        System.out.println("Length of Line is "+Math.round(lineLengthCalculate(-5,4,-3,-1)));
+        double firstLength = lineLengthCalculate(-5, 4, -3, -1);
+        double secondLength = lineLengthCalculate(5, 7, 5, 9);
+        if (Objects.equals(firstLength, secondLength)) {
+            System.out.println("The Line Are Equal");
+            System.out.println("Line One Value " + firstLength + "\nLine Two Value " + secondLength);
+        } else {
+            System.out.println("The Are Not Equal");
+            System.out.println("Line One Value " + firstLength + "\nLine Two Value " + secondLength);
+        }
     }
 
     private static double lineLengthCalculate(int x1, int x2, int y1, int y2) {
-        return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        double x = Math.pow(x2 - x1, 2);
+        double y = Math.pow(y2 - y1, 2);
+        return Math.sqrt(x + y);
     }
 }
