bdc243752306c4e549feec8b4a346a74820bb05d
Prashant Deepake <deepakeprashant1@gmail.com>
2022-12-29 22:18:33 +0530

    LINE_COMPARE_USING_CompareTo_METHOD

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -5,12 +5,23 @@ public class LineComparison {
         System.out.println("Welcome To Line Comparison Computation");
         double firstLength = lineLengthCalculate(-5, 4, -3, -1);
         double secondLength = lineLengthCalculate(5, 7, 5, 9);
+        String line1 = String.valueOf(firstLength);
+        String line2 = String.valueOf(secondLength);
+        int result = line1.compareTo(line2);
         if (Objects.equals(firstLength, secondLength)) {
             System.out.println("The Line Are Equal");
             System.out.println("Line One Value " + firstLength + "\nLine Two Value " + secondLength);
         } else {
             System.out.println("The Are Not Equal");
             System.out.println("Line One Value " + firstLength + "\nLine Two Value " + secondLength);
+        }
+        if (result==0){
+            System.out.println("Both Line are Equals");
+        } else if (result>0) {
+            System.out.println("The Line 1 is Greater Than Line 2");
+        }
+        else {
+            System.out.println("The Line 2 is Greater Than Line 1");
         }
     }
 
